da7596deec093e191ab2c0a87e452d4df3d56f30
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-09-29 00:27:39 -0300

    fix

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function NotFoundPage() {
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
+        <div className="min-h-svh flex flex-col items-center justify-center bg-gray-50 p-6">
             <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center">404 <br/> Página no encontrada</h1>
             <p className="text-gray-600 text-lg mb-8">
                 Lo sentimos, la página que estás buscando no existe.
