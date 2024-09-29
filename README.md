6cbb5d4cfa2311a221df7b523cb6ac974355b2ad
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-09-29 00:37:38 -0300

    readme & users

--- README.md
+++ README.md
@@ -1,36 +1,24 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# 🏦 Home Banking React - Sprint 2 - ITBA
 
-## Getting Started
+![Logo Rossum](https://itbank.moveup.digital/img/logo.webp)
 
-First, run the development server:
+## Visit the website
+[Open site](https://itbank-nextjs.moveup.digital/)
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+## User & Password:
+User
+```
+juliana.martinez
+```
+Password
+```
+Password123
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## 👥 Autores
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [Agustina Méndez](https://github.com/aguiita)
+- [Gustavo David Baya Vargas](https://github.com/gustavobaya)
+- [Octavio Medina](https://github.com/ocmedina)
+- [Maximiliano Vecchio](https://www.linkedin.com/in/maxivecchio)
+- [Magali Plotek](https://www.linkedin.com/in/magal%C3%AD-plotek-a52b13277)
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,77 +1,89 @@
 "use client";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useRouter } from "next/navigation";
-import { useUser } from "@/context/UserContext";
+import {useState} from "react";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {useRouter} from "next/navigation";
+import {useUser} from "@/context/UserContext";
 
 function LoginForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter();
-  const { login } = useUser();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+    const {login} = useUser();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setError("");
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError("");
 
-    const success = await login(username, password);
-    if (success) {
-      router.push("/"); // Redirigir al home si el login es exitoso
-    } else {
-      setError("Usuario o contraseña incorrectos");
-    }
-  };
+        const success = await login(username, password);
+        if (success) {
+            router.push("/"); // Redirigir al home si el login es exitoso
+        } else {
+            setError("Usuario o contraseña incorrectos");
+        }
+    };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-700">
-      <div className="relative flex flex-col m-6 space-y-8 bg-black shadow-2xl rounded-2xl md:flex-row md:space-y-0 w-full max-w-5xl">
-        <div className="flex flex-col justify-center p-8 md:p-14 w-full md:max-w-md">
-          <div className="flex">
-            <img className="h-6 w-12" src="/logo.webp" alt="Rossum Logo" loading="lazy" />
-            <span className="font-bold ml-2 text-lg hidden md:inline-block text-white">
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-slate-700">
+            <div
+                className="relative flex flex-col m-6 space-y-8 bg-black shadow-2xl rounded-2xl md:flex-row md:space-y-0 w-full max-w-5xl">
+                <div className="flex flex-col justify-center p-8 md:p-14 w-full md:max-w-md">
+                    <div className="flex">
+                        <img className="h-6 w-12" src="/logo.webp" alt="Rossum Logo" loading="lazy"/>
+                        <span className="font-bold ml-2 text-lg hidden md:inline-block text-white">
               <span className="logo_name text-white">Rossum | </span>HomeBanking
             </span>
-          </div>
+                    </div>
 
-          <form onSubmit={handleLogin} className="space-y-4">
-            <div className="my-2">
-              <Label htmlFor="user" className="text-white">Usuario</Label>
-              <Input
-                id="user"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className="my-2">
-              <Label htmlFor="password" className="text-white">Contraseña</Label>
-              <Input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            {error && <p className="text-red-500">{error}</p>}
-            <Button type="submit" className="mt-4">Iniciar sesión</Button>
-          </form>
-        </div>
+                    <form onSubmit={handleLogin} className="gap-y-4">
+                        <div className={"text-white opacity-60 p-2 bg-white/20 rounded-lg mt-2"}>
+                            <span className="font-semibold">Credenciales de prueba:</span> <br/>
+                            <div className={"flex flex-col sm:flex-row sm:gap-4"}>
+                            <span>
+              User: juliana.martinez
+              </span>
+                                <span>
+              Passowrd: Password123
+                </span>
+                            </div>
+                        </div>
+                        <div className="my-2">
+                            <Label htmlFor="user" className="text-white">Usuario</Label>
+                            <Input
+                                id="user"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="my-2">
+                            <Label htmlFor="password" className="text-white">Contraseña</Label>
+                            <Input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        {error && <p className="text-red-500">{error}</p>}
+                        <Button type="submit" className="mt-4">Iniciar sesión</Button>
+                    </form>
+                </div>
 
-        <div className="relative">
-          <img
-            src="/oficina.avif"
-            alt="img"
-            className="h-full w-full hidden rounded-r-2xl md:block object-cover"
-             loading="lazy"
-          />
+                <div className="relative">
+                    <img
+                        src="/oficina.avif"
+                        alt="img"
+                        className="h-full w-full hidden rounded-r-2xl md:block object-cover"
+                        loading="lazy"
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default LoginForm;
